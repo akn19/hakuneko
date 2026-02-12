b28b3582588d3b5259668ffc18e1fa0e9d01e673
akn19 <86169232+akn19@users.noreply.github.com>
2026-02-12 12:18:28 +0000

    Add Lunar Animes connector

---
+++ src/web/mjs/connectors/LunarAnimes.mjs
@@ -0,0 +1,66 @@
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
+
+export default class LunarAnimes extends Connector {
+
+    constructor() {
+        super();
+        super.id = 'lunaranimes';
+        super.label = 'Lunar Animes';
+        this.tags = ['manga', 'manhwa', 'manhua', 'multi-lingual', 'aggregator'];
+        this.url = 'https://lunaranime.ru';
+        this.apiUrl = 'https://api.lunaranime.ru/api/manga/';
+
+        this.languageMap = {
+            'zh': 'Chinese',
+            'en': 'English',
+            'id': 'Indonesian',
+            'ja': 'Japanese',
+            'ko': 'Korean',
+            'pl': 'Polish',
+            'pt': 'Portuguese',
+            'ru': 'Russian',
+            'es': 'Spanish',
+            'th': 'Thai',
+            'tr': 'Turkish',
+            'vi': 'Vietnamese',
+        };
+    }
+
+    canHandleURI(uri) {
+        return /https?:\/\/lunaranime\.ru\/manga\/[^/]+/.test(uri.href);
+    }
+
+    async _getMangaFromURI(uri) {
+        const slug = uri.pathname.split('/').pop();
+        const request = new Request(new URL(this.url + uri.pathname), this.requestOptions);
+        const data = await this.fetchDOM(request, 'meta[property="og:title"]');
+        const title = data[0].content.trim();
+        return new Manga(this, slug, title);
+    }
+
+    async _getMangas() {
+        const request = new Request(new URL('search?', this.apiUrl), this.requestOptions);
+        const { manga } = await this.fetchJSON(request);
+        return manga.map(entry => ({
+            id: entry.slug,
+            title: entry.title,
+        }));
+    }
+
+    async _getChapters(manga) {
+        const request = new Request(new URL(manga.id, this.apiUrl), this.requestOptions);
+        const { data } = await this.fetchJSON(request);
+        return data.map(chapter => ({
+            id: `${manga.id}/${chapter.chapter_number}?language=${chapter.language}`,
+            title: `Chapter ${chapter.chapter_number} (${chapter.language})`,
+            language: chapter.language,
+        }));
+    }
+
+    async _getPages(chapter) {
+        const request = new Request(new URL(chapter.id, this.apiUrl), this.requestOptions);
+        const { data: { images } } = await this.fetchJSON(request);
+        return images;
+    }
+}
