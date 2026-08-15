429bb6b7884a813589d3675c52256cdc08fcb803
akn19 <86169232+akn19@users.noreply.github.com>
2026-08-15 23:32:17 +0700

    FIX SoulsSans

--- src/web/mjs/connectors/SoulsSans.mjs
+++ src/web/mjs/connectors/SoulsSans.mjs
@@ -1,13 +1,66 @@
-import WordPressMangastream from './templates/WordPressMangastream.mjs';
+import Connector from '../engine/Connector.mjs';
+import Manga from '../engine/Manga.mjs';
 
-export default class SoulsSans extends WordPressMangastream {
+export default class SoulsSans extends Connector {
 
     constructor() {
         super();
         super.id = 'soulscans';
-        super.label = 'SoulsSans';
-        this.tags = ['manga', 'indonesian'];
-        this.url = 'https://soulscans.my.id';
-        this.path = '/manga/list-mode/';
+        super.label = 'Soul Scans';
+        this.tags = ['manga', 'manhwa', 'manhua', 'indonesian'];
+        this.url = 'https://v1.soulscans.org';
+        this.api = 'https://img.soulscans.org/api/';
+    }
+
+    async _getMangaFromURI(uri) {
+        const slug = uri.pathname.split('/').pop();
+        const request = new Request(new URL(`series/comic/${slug}`, this.api), this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return new Manga(this, data.slug, data.title.trim());
+    }
+
+    async _getMangas() {
+        let mangaList = [];
+        for (let page = 1, run = true; run; page++) {
+            const mangas = await this._getMangasFromPage(page);
+            if (mangas.length === 0) {
+                run = false;
+            } else {
+                mangaList.push(...mangas);
+            }
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page) {
+        const uri = new URL('search', this.api);
+        uri.searchParams.set('type', 'COMIC');
+        uri.searchParams.set('page', page);
+        uri.searchParams.set('limit', '100');
+        const request = new Request(uri, this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return data.data.map(entry => {
+            return {
+                id: entry.slug,
+                title: entry.title.trim()
+            };
+        });
+    }
+
+    async _getChapters(manga) {
+        const request = new Request(new URL(`series/comic/${manga.id}`, this.api), this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return data.units.map(unit => {
+            return {
+                id: unit.slug,
+                title: unit.title
+            };
+        });
+    }
+
+    async _getPages(chapter) {
+        const request = new Request(new URL(`series/comic/${chapter.manga.id}/chapter/${chapter.id}`, this.api), this.requestOptions);
+        const data = await this.fetchJSON(request);
+        return data.chapter.pages.map(page => page.image_url.replace(/^http:/, 'https:'));
     }
 }
